d2354db69f55e7de02149204fcba180eb222cab4
Sigurdur Arnason <shamaroth@gmail.com>
2021-03-04 01:21:03 +0000

    Functional and Styled

--- src/App.js
+++ src/App.js
@@ -1,90 +1,105 @@
-import React from "react";
-import "./App.css";
+import React, { useState } from "react";
+import styled, { css } from "styled-components";
+
+// import "./App.css";
+
 import { Button } from "./components";
+import { Input } from "./components";
 
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      value: "",
-      list: [],
-      cost: 0,
-      sum: 0,
-    };
+// Styles
 
-    this.handleOnClick = this.handleOnClick.bind(this);
-    this.handleOnChangeValue = this.handleOnChangeValue.bind(this);
-    this.handleOnChangeCost = this.handleOnChangeCost.bind(this);
-  }
+const Container = styled.div`
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-start;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`;
+const Column = styled.div``;
+const Section = styled.div`
+  text-align: center;
+  margin: 5vh;
+  width: 100%;
+  padding: 10px;
+`;
+const ListItems = styled.ul`
+  text-align: left;
+  background-color: #1f2126;
+  margin: 0;
+  position: relative;
+  list-style-type: none;
+  padding-left: 2.5rem;
+  margin-bottom: 0.5rem;
+`;
 
-  handleOnChangeValue(event) {
-    this.setState({ value: event.target.value });
-  }
-  handleOnChangeCost(event) {
-    this.setState({ cost: Number(event.target.value) });
-  }
+// Logic
 
-  handleOnClick() {
-    this.setState({
-      list: this.state.list.concat(this.state.value),
-      value: "",
-      sum: this.state.sum + this.state.cost,
-      cost: "",
-    });
-  }
+const data = [];
 
-  //  this.setState((state) => {
-  //    const list = state.list.concat(state.value);
-  //    const value = "";
-  //    const sum = state.sum + state.cost;
-  //    const cost = 0;
-  //    return { list, value, cost, sum };
-  //  });
+const App = () => {
+  const [stateList, setList] = useState([]);
+  const [stateValue, setValue] = useState("");
+  const [stateCost, setCost] = useState(0);
+  const [stateSum, setSum] = useState(0);
 
-  render() {
-    console.log("Render");
-    return (
-      <div className="App-header">
-        <span>
-          Item Name:
-          <input
+  const handleOnChangeValue = (event) => {
+    console.log(stateValue + " : " + stateCost);
+    setValue(event.target.value);
+  };
+
+  const handleOnChangeCost = (event) => {
+    console.log(stateValue + " : " + stateCost);
+    setCost(Number(event.target.value));
+  };
+
+  const handleOnClick = () => {
+    console.log("onClick: " + stateValue + stateCost);
+
+    setList(data.push([stateValue, stateCost]));
+    setCost(0);
+    setValue("");
+    data.forEach((item) => setSum(stateSum + item[1]));
+
+    console.log("OnClick Over: " + stateValue + stateCost);
+  };
+
+  return (
+    <Container>
+      <Column>
+        <Section>
+          <Input
+            onChange={handleOnChangeValue}
+            value={stateValue}
             type="text"
-            value={this.state.value}
-            onChange={this.handleOnChangeValue}
           />
-        </span>
-        <span>
-          Item Cost:
-          <input
+          <Input
+            onChange={handleOnChangeCost}
+            value={stateCost}
             type="number"
-            value={this.state.cost}
-            onChange={this.handleOnChangeCost}
-          />
-        </span>
-
-        <Button handleOnClick={this.handleOnClick} />
-        <p>Cost: {Number(this.state.sum)}</p>
-        <ul>
-          {" "}
-          Items:
-          {this.state.list.map((item) => (
-            <li key={item}>{item}</li>
-          ))}
-        </ul>
-      </div>
-    );
-  }
-}
-
+          />{" "}
+          <Button handleOnClick={handleOnClick} />
+        </Section>
+      </Column>
+      <Column>
+        <Section>
+          <ListItems>
+            <p>Items</p>
+            <table>
+              {data.map((item) => (
+                <tr>
+                  <td key={item[0]}>{item[0]}</td>
+                  <td key={item[1]}>{item[1]}</td>
+                </tr>
+              ))}
+              <p>Total Expenses: {stateSum}</p>
+            </table>
+          </ListItems>
+        </Section>
+      </Column>
+    </Container>
+  );
+};
 export default App;
-
-// <Form>
-// <Button handleOnClick={this.handleOnClick} />
-// <input type="text" value={this.state.valu}></input>
-// <p>{this.state.displayValue}</p>
-// <ul>
-//   {this.state.list.map((item) => (
-//     <li key={item}>{item}</li>
-//   ))}
-// </ul>
-// </Form>
